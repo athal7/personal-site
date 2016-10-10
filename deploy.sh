42cfd3d5082ad9e570d0108d0a781d5b17a6697d
Andrew Thal <hello@athal7.com>
2016-10-10 12:27:57 -0400

    Handle multiple aws profiles

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 npm run prod && \
-aws s3 sync app/assets/images s3://athal7com/images && \
-aws s3 sync public s3://athal7com
+aws s3 --profile home sync app/assets/images s3://athal7com/images && \
+aws s3 --profile home sync public s3://athal7com
