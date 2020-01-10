4f28be26c40a23e4d48650f48ca76678f9bd3ca3
Andrew Thal <andrewthal88@gmail.com>
2020-01-09 21:10:25 -0500

    Try fix styles

--- app/templates/App.jsx
+++ app/templates/App.jsx
@@ -1,56 +1,63 @@
-import React, {Component} from 'react'
-import Header from '../components/header/Header'
-import Filters from '../components/filters/Filters'
-import Carousel from '../components/carousel/Carousel'
-import Contacts from '../components/contacts/Contacts'
-import {CAROUSEL_ITEMS, FILTERS, CONTACTS} from '../data/carousel'
-import s from './style.css'
+import React, { Component } from "react";
+import Header from "../components/header/Header";
+import Filters from "../components/filters/Filters";
+import Carousel from "../components/carousel/Carousel";
+import Contacts from "../components/contacts/Contacts";
+import { CAROUSEL_ITEMS, FILTERS, CONTACTS } from "../data/carousel";
+import s from "../styles/style.css";
 
 export default class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       filter: null,
       carouselItems: CAROUSEL_ITEMS,
       lead: CAROUSEL_ITEMS[0].description
-    }
+    };
   }
 
   render() {
     return (
       <div className={s.app}>
-        <Header
-          name="Andrew Thal"
-          lead={this.state.lead} />
+        <Header name="Andrew Thal" lead={this.state.lead} />
 
         <Filters
           filters={FILTERS}
           onClick={this.handleFilterClick.bind(this)}
-          currentFilter={this.state.filter} />
+          currentFilter={this.state.filter}
+        />
 
         <Carousel
           carouselItems={this.state.carouselItems}
-          afterChange={this.carouselChange.bind(this)} />
+          afterChange={this.carouselChange.bind(this)}
+        />
 
-        <Contacts
-          contacts={CONTACTS} />
+        <Contacts contacts={CONTACTS} />
       </div>
-    )
+    );
   }
 
   handleFilterClick(event) {
-    let filter = event.target.getAttribute("data-filter")
-    let carouselItems = CAROUSEL_ITEMS
-    if (filter && this.state.filter == filter) { filter = null }
-    if (filter) { carouselItems = carouselItems.filter((i) => i.category == filter) }
+    let filter = event.target.getAttribute("data-filter");
+    let carouselItems = CAROUSEL_ITEMS;
+    if (filter && this.state.filter == filter) {
+      filter = null;
+    }
+    if (filter) {
+      carouselItems = carouselItems.filter(i => i.category == filter);
+    }
     this.setState({
       filter: filter,
       carouselItems: carouselItems,
       lead: carouselItems[0].description
-    })
+    });
   }
 
   carouselChange(index) {
-    this.setState({lead: document.querySelector(`.slick-slide[data-index="${index}"] .carousel-item`).getAttribute('data-description')})
+    this.setState({
+      lead: document
+        .querySelector(`.slick-slide[data-index="${index}"] .carousel-item`)
+        .getAttribute("data-description")
+    });
   }
 }
