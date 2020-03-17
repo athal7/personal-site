a8a9875d24d825597e3097cd0c57512c43929c4c
Andrew Thal <andrewthal88@gmail.com>
2020-03-17 16:21:38 -0400

    Update progress indicator

--- app/assets/remark.js
+++ app/assets/remark.js
@@ -2,4 +2,9 @@ remark.create({
   navigation: { scroll: false, click: false },
   highlightLines: true,
   highlightStyle: 'github',
+  slideNumberFormat: function (current, total) {
+    var percentComplete = Math.round(current / total * 100);
+    return "<div id='progress'><div id='progress-bar' style='width:" + percentComplete + "%'></div></div>";
+  },
+  countIncrementalSlides: false
 });
