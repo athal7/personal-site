7f074afbb6242df8e23c3a2556a5b9d54c036bd4
Andrew Thal <andrewthal88@gmail.com>
2021-01-18 09:20:48 -0500

    Fix analytics, add webvitals

--- app/initialize.js
+++ app/initialize.js
@@ -1,12 +1,29 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
+import ReactGA from 'react-ga'
+import {getCLS, getFID, getLCP} from 'web-vitals'
 import App from './templates/App'
 import data from './data/carousel'
 
+const debug = !!window.location.host.match('localhost')
 
+const sendToGoogleAnalytics = ({name, delta, id}) => {
+  ReactGA.event({
+    category: 'Web Vitals',
+    action: name,
+    label: id,
+    value: Math.round(name === 'CLS' ? delta * 1000 : delta),
+    nonInteraction: true,
+  })
+}
 
-window.initialize = function(person, name) { 
+window.initialize = function(person, name, analyticsId) { 
+  ReactGA.initialize(analyticsId, {debug})
+  ReactGA.pageview(window.location.pathname + window.location.search)
   document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render(<App data={data[person]} name={name} />, document.querySelector('#app'))
+    ReactDOM.render(<App data={data[person]} name={name} />, document.querySelector('#app'))  
+    getCLS(sendToGoogleAnalytics)
+    getFID(sendToGoogleAnalytics)
+    getLCP(sendToGoogleAnalytics)
   })
 }
