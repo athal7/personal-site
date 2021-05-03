db7a251679e706e389c0d21f2ad2ead18a979d48
Andrew Thal <467872+athal7@users.noreply.github.com>
2021-05-03 14:51:35 +0000

    Switch to webpack
    Signed-off-by: GitHub <noreply@github.com>

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const fs = require('fs');
+
+const templates = fs.readdirSync(__dirname + '/app/assets')
+                    .filter(f => !f.startsWith('_') && f.endsWith('.jade'))
+                    .map(f => f.split('.')[0])
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    app: './app/initialize.js',
+    remark: './app/assets/remark.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',         
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.jade$/,
+        use: ['pug-loader']
+      },
+    ],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public'),
+    clean: true,
+    publicPath: '/',
+  },
+  plugins: [
+    ...templates.map(name => (
+      new HtmlWebpackPlugin({
+        inject: false,
+        minify: false,
+        template: `!!pug-loader!app/assets/${name}.jade`,
+        filename: `${name}.html`
+      })
+    )),
+    new CopyWebpackPlugin({
+      patterns: [{
+        from: 'app/assets/images', 
+        to: 'images',
+      }],
+    }),
+  ],
+  devServer: {
+    contentBase: './public',
+    writeToDisk: true
+  }
+};
